aefda8f1454209b6b97691fff6429a89f898a6e2
Erza Fahmi Fasya <58721335+erzaffasya@users.noreply.github.com>
2023-04-02 13:10:12 +0800

    Update nodemailer.js

--- helpers/nodemailer.js
+++ helpers/nodemailer.js
@@ -1,13 +1,26 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
-const transporter = nodemailer.createTransport({
-    service: process.env.HOST_TRANSPORTER,
-    auth: {
-        user: process.env.EMAIL_TRANSPORTER,
-        pass: process.env.PASS_TRANSPORTER
-    }
-});
+
+// const transporter = nodemailer.createTransport({
+//     service: c,
+//     auth: {
+//         user: process.env.EMAIL_TRANSPORTER,
+//         pass: process.env.PASS_TRANSPORTER
+//     }
+// });
+
+var smtpConfig = {
+  host: process.env.HOST_TRANSPORTER,
+  port: 465,
+  // secure: true, // use SSL
+  auth: {
+      user: process.env.EMAIL_TRANSPORTER,
+      pass: process.env.PASS_TRANSPORTER
+  }
+};
+var transporter = nodemailer.createTransport(smtpConfig);
+
 // verify connection configuration
 transporter.verify(function (error, success) {
     if (error) {
