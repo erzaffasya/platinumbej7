75acf490cc17585deb21e1bb6f7ad81a94a37740
fattahjundi <fattahjundi97@gmail.com>
2023-04-06 21:05:04 +0700

    [fattah] add socket

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const swaggerDoc = require('./swagger.json');
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.set('views', './views')
 app.set('view engine', 'ejs');
 app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc))
 app.use('/api', routes);
@@ -27,9 +28,46 @@ app.use((err, req, res, next) => {
   })
 })
 
+// Chat
+const rooms = { name: {} }
+
+app.get('/api/chat', (req, res) => {
+    res.render('index', { rooms: rooms })
+})
+
+app.get('/api/chat/:room', (req, res) => {
+    const { room } = req.params
+    if(rooms[room] == null) {
+        return res.redirect('/api/chat')
+    }
+    res.render('chatroom', { roomName: req.params.room })
+})
+
+app.post('/api/chat/room', (req, res) => {
+    const { room } = req.body
+    if(rooms[room] != null) {
+        return res.redirect('/api/chat')
+    }
+    rooms[room] = { users: {} }
+    res.redirect(`/api/chat/${room}`)
+    // send message that new room created
+})
+
+io.on('connection', (socket) => {
+    // console.log(socket.adapter.rooms);
+    console.log(`a user connected with id ${socket.id}`);
+  
+    socket.on('chat message', (msg) => {
+        // console.log(`message: ${msg}`);
+        socket.broadcast.emit('new chat', msg)
+    })
+  
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    })
+})
+
 // server
 server.listen(port, () => {
   console.log(`Port running on localhost:${port}`);
 });
-
-
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const user = require('./userRoute');
 const order = require('./orderRoute');
 const product = require('./productRoute');
+const chat = require('./chatRoute');
 
 router.get('/home', (req, res) => {
   res.render('home');
@@ -11,5 +12,6 @@ router.get('/home', (req, res) => {
 router.use('/', user);
 router.use('/', order);
 router.use('/', product);
+router.use('/', chat);
 
 module.exports = router;
